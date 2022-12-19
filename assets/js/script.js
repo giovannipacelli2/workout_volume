69b44cad8010035e73fbb057ce5e899b5709f1a5
giovannipacelli2 <giovannipacelli2@gmail.com>
2022-12-19 20:41:19 +0100

    Build interface

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,31 @@
 
 let form = document.forms.data;
 
+let firstTime = false;
+
+form.onsubmit = function (e){
+
+    if ( !firstTime ) {
+        const table = "<table id='workout-table'><thead><tr><th>Nome esercizio</th><th>Carico da utilizzare</th></tr></thead><tbody id='body'></tbody></table>";
+
+        form.insertAdjacentHTML('afterend', table);
+
+        firstTime = true;
+    }
+
+    const table = document.body.querySelector("#workout-table");
+
+    insertTable( table, form.exercise.value, form.weight.value );
+
+
+    return false;
+
+};
+
+function insertTable( table, name, weight ) {
+
+    let html = `<tr><td>${name}</td><td>${weight} kg</td>`;
+
+    table.querySelector("#body").insertAdjacentHTML('beforeend', html);
+
+}
